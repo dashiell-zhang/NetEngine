6b4b6979012b286a28c7fa9938c1ef8f598c02b4
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-03-25 22:35:58 +0800

    DataTable , List , Excel 数据转换方法实现

---
+++ Methods/IO/Excel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods.IO
+{
+    class Excel
+    {
+
+    }
+}
---
+++ Methods/IO/NpoiMemoryStream.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Methods.IO
+{
+    //新建类 重写Npoi流方法(原始方法会在导出2007版本时出现数据流中断的问题)
+    public class NpoiMemoryStream : MemoryStream
+    {
+        public NpoiMemoryStream()
+        {
+            AllowClose = true;
+        }
+
+        public bool AllowClose { get; set; }
+
+        public override void Close()
+        {
+            if (AllowClose)
+                base.Close();
+        }
+    }
+}

9055cf799a3849c0ab44548b5e578504c6fdb373
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-04-03 16:00:09 +0800

    为接口添加跨域方法功能，添加 get 请求方法，及请求 demo

---
+++ WebApi/Controllers/ApiController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Methods;
+using WebApi.Actions;
+using Methods.Json;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+        [HttpGet]
+        public RunResult Get(string json)
+        {
+            RunResult result = new RunResult();
+
+            if (json != null)
+            {
+
+                Run run = JsonHelper.JSONToObject<Run>(json);
+
+                if (Public.GetKey(run) == run.key)
+                {
+                    switch (run.run)
+                    {
+
+                        case "Test":
+                            return TestAction.Run(run.data);
+
+
+                        default:
+                            result.status = 0;
+                            result.msg = "未找到" + run.run + "方法";
+                            break;
+                    }
+                }
+                else
+                {
+                    result.status = 0;
+                    result.msg = "data与key不匹配";
+                }
+            }
+            else
+            {
+                result.status = 0;
+                result.msg = "未收到任何数据";
+            }
+            return result;
+        }
+
+
+
+
+        [HttpPost]
+        public RunResult Post([FromBody]Run run)
+        {
+            RunResult result = new RunResult();
+
+            if (Public.GetKey(run) == run.key)
+            {
+                switch (run.run)
+                {
+                    
+                    case "Test":
+                        return TestAction.Run(run.data);
+
+
+                    default:
+                        result.status = 0;
+                        result.msg = "未找到" + run.run + "方法";
+                        break;
+                }
+            }
+            else
+            {
+                result.status = 0;
+                result.msg = "data与key不匹配";
+            }
+
+            return result;
+        }
+
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -26,6 +26,9 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //注册跨域信息
+            services.AddCors(option => option.AddPolicy("cors", policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +43,9 @@ namespace WebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //注册跨域信息
+            app.UseCors("cors");
 
             app.UseHttpsRedirection();
             app.UseMvc();

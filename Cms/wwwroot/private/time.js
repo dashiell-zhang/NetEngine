ebeabfafcdd1e6b6d883345236d37dca2fe0356e
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-04-28 18:14:25 +0800

    引入 laydate 时间组件，添加 time 类型数据 js 换成操作方法

---
+++ Cms/wwwroot/private/time.js
@@ -0,0 +1,32 @@
+﻿function timeTosec (time) {
+    var s = '';
+
+    var hour = time.split(':')[0];
+    var min = time.split(':')[1];
+    var sec = time.split(':')[2];
+
+    s = Number(hour * 3600) + Number(min * 60) + Number(sec);
+
+    return s;
+};
+
+
+function secTotime (s) {
+    var t;
+    if (s > -1) {
+        var hour = Math.floor(s / 3600);
+        var min = Math.floor(s / 60) % 60;
+        var sec = s % 60;
+        if (hour < 10) {
+            t = '0' + hour + ":";
+        } else {
+            t = hour + ":";
+        }
+
+        if (min < 10) { t += "0"; }
+        t += min + ":";
+        if (sec < 10) { t += "0"; }
+        t += sec.toFixed(2);
+    }
+    return t.substring(0, 8);
+};

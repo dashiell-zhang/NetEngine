1ede82305059b0dd9a80119a89d3f418c5f15452
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-07-09 15:30:45 +0800

    添加 FileInfo 和 PageList 基础类

---
+++ Models/Dtos/dtoFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Dtos
+{
+
+
+    /// <summary>
+    /// 文件信息
+    /// </summary>
+    public class dtoFileInfo
+    {
+
+        /// <summary>
+        /// 文件ID
+        /// </summary>
+        public string fileid { get; set; }
+
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string filename { get; set; }
+
+    }
+}
---
+++ Models/Dtos/dtoPageList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Dtos
+{
+
+
+    /// <summary>
+    /// 分页数据信息基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class dtoPageList<T>
+    {
+
+
+        /// <summary>
+        /// 数据总量
+        /// </summary>
+        public int total { get; set; }
+
+
+
+
+        /// <summary>
+        /// 具体数据内容
+        /// </summary>
+        public List<T> list { get; set; }
+    }
+}

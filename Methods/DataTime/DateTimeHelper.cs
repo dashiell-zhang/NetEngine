50f2608233e0bd7677a951029d6452e82ce38848
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-07-17 13:47:02 +0800

    添加时间操作类 实现时间和时间戳的相互转换，集成 JWT Token 生成方法

---
+++ Methods/DataTime/DateTimeHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods.DataTime
+{
+    public class DateTimeHelper
+    {
+
+
+        /// <summary>
+        /// unix时间戳转DateTime
+        /// </summary>
+        /// <param name="unix"></param>
+        /// <returns></returns>
+        public static DateTime UnixToTime(long unix)
+        {
+            System.DateTime startTime = TimeZoneInfo.ConvertTimeToUtc((new DateTime(1970, 1, 1)).ToLocalTime()).ToLocalTime(); // 当地时区
+            return startTime.AddSeconds(unix);
+        }
+
+
+
+
+        /// <summary>
+        /// DateTime转unix时间戳
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static long TimeToUnix(DateTime time)
+        {
+            System.DateTime startTime = TimeZoneInfo.ConvertTimeToUtc((new DateTime(1970, 1, 1)).ToLocalTime()).ToLocalTime(); // 当地时区
+            return (long)(time - startTime).TotalSeconds; // 相差秒数
+        }
+
+
+
+        /// <summary>
+        /// JavaScript时间戳转DateTime
+        /// </summary>
+        /// <param name="unix"></param>
+        /// <returns></returns>
+        public static DateTime JsToTime(long js)
+        {
+           
+            System.DateTime startTime = TimeZoneInfo.ConvertTimeToUtc((new DateTime(1970, 1, 1)).ToLocalTime()).ToLocalTime(); // 当地时区
+            return startTime.AddMilliseconds(js);
+        }
+
+
+
+
+        /// <summary>
+        /// DateTime转Js时间戳
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static long TimeToJs(DateTime time)
+        {
+            System.DateTime startTime = TimeZoneInfo.ConvertTimeToUtc((new DateTime(1970, 1, 1)).ToLocalTime()).ToLocalTime(); // 当地时区
+            return (long)(time - startTime).TotalMilliseconds;
+        }
+
+
+    }
+}
--- Methods/Verify/JwtToken.cs
+++ Methods/Verify/JwtToken.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Models.JwtBearer;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 
 namespace Methods.Verify
@@ -31,6 +35,33 @@ namespace Methods.Verify
             {
                 return null;
             }
+        }
+
+
+
+        /// <summary>
+        /// 生成一个Token
+        /// </summary>
+        /// <returns></returns>
+        public static string GetToken(Claim[] claims)
+        {
+            var conf = Methods.Start.StartConfiguration.configuration;
+
+            var jwtSettings = new JwtSettings();
+            conf.Bind("JwtSettings", jwtSettings);
+
+            //对称秘钥
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey));
+
+            //签名证书(秘钥，加密算法)
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //生成token
+            var token = new JwtSecurityToken(jwtSettings.Issuer, jwtSettings.Audience, claims, DateTime.Now, DateTime.Now.AddMinutes(30), creds);
+
+            var ret = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return ret;
         }
 
     }

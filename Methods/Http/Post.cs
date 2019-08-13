7b34238c96e3d24fdb355aa5ffb7c2b4ecb6cb2d
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-08-13 17:34:09 +0800

    异步 post 方法实现

--- Methods/Http/Post.cs
+++ Methods/Http/Post.cs
@@ -9,7 +9,13 @@ namespace Methods.Http
     public class Post
     {
 
-        //Post数据到指定url
+        /// <summary>
+        /// Post数据到指定url
+        /// </summary>
+        /// <param name="Url">Url</param>
+        /// <param name="Data">数据</param>
+        /// <param name="type">form,data,json,xml</param>
+        /// <returns></returns>
         public static string Run(string Url, string Data, string type)
         {
 
@@ -45,6 +51,50 @@ namespace Methods.Http
             res.Close();
 
             return PostJie;
+        }
+
+
+        /// <summary>
+        /// Post数据到指定url,异步执行
+        /// </summary>
+        /// <param name="Url">Url</param>
+        /// <param name="Data">数据</param>
+        /// <param name="type">form,data,json,xml</param>
+        /// <returns></returns>
+        public async static void RunAsync(string Url, string Data, string type)
+        {
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(Url);
+            byte[] requestBytes = System.Text.Encoding.UTF8.GetBytes(Data);
+            req.Method = "POST";
+            if (type == "form")
+            {
+                req.ContentType = "application/x-www-form-urlencoded";
+            }
+            else if (type == "data")
+            {
+                req.ContentType = "multipart/form-data";
+            }
+            else if (type == "json")
+            {
+                req.ContentType = "application/json";
+            }
+            else if (type == "xml")
+            {
+                req.ContentType = "text/xml";
+            }
+
+            req.ContentLength = requestBytes.Length;
+            Stream requestStream = await req.GetRequestStreamAsync();
+            requestStream.Write(requestBytes, 0, requestBytes.Length);
+            requestStream.Close();
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader sr = new StreamReader(res.GetResponseStream(), System.Text.Encoding.UTF8);
+            string PostJie = sr.ReadToEnd();
+            sr.Close();
+            res.Close();
+
         }
     }
 

9e4988b1d2144a864fd599c63b92d53882d9cd94
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-10-28 15:19:31 +0800

    微信 H5 开发，JsSDK 签名计算方法实现

---
+++ Methods/WeiXin/H5/WeiXinHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Methods.Http;
+using System.Linq;
+using System.Security.Cryptography;
+using Methods.WeiXin.H5.Models;
+
+namespace Methods.WeiXin.H5
+{
+
+    public class WeiXinHelper
+    {
+
+
+        private static string appid;
+
+        private static string appsecret;
+
+
+
+        /// <summary>
+        /// 微信H5网页开发帮助类
+        /// </summary>
+        /// <param name="in_appid">微信公众号APPID</param>
+        /// <param name="in_appsecret">微信公众号密钥</param>
+        public WeiXinHelper(string in_appid, string in_appsecret)
+        {
+            appid = in_appid;
+            appsecret = in_appsecret;
+        }
+
+
+
+        /// <summary>
+        /// 获取 AccessToken
+        /// </summary>
+        /// <returns></returns>
+        public string GetAccessToken()
+        {
+
+            string url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + appsecret;
+
+            var returnJson = Http.Post.Run(url, "", "form");
+
+            var token = Json.JsonHelper.GetValueByKey(returnJson, "access_token");
+
+            return token;
+        }
+
+
+
+        /// <summary>
+        /// 获取 TicketID
+        /// </summary>
+        /// <returns></returns>
+        private string GetTicketID()
+        {
+
+            string getUrl = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + GetAccessToken() + "&type=jsapi";
+
+            string returnJson = Http.Post.Run(getUrl, "", "form");
+
+            var ticketid = Json.JsonHelper.GetValueByKey(returnJson, "ticket");
+
+            return ticketid;
+        }
+
+
+
+        /// <summary>
+        /// 获取 JsSDK 签名信息
+        /// </summary>
+        /// <returns></returns>
+        public WeiXinJsSdkSign GetJsSDKSign()
+        {
+            WeiXinJsSdkSign sdkSign = new WeiXinJsSdkSign();
+
+            sdkSign.appid = appid;
+            string url = HttpContext.GetUrl();
+            sdkSign.timestamp = Methods.DataTime.DateTimeHelper.TimeToUnix(DateTime.Now);
+            sdkSign.noncestr = Guid.NewGuid().ToString().Replace("-", "");
+            string jsapi_ticket = GetTicketID();
+
+            string strYW = "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + sdkSign.noncestr + "&timestamp=" + sdkSign.timestamp + "&url=" + url;
+
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] bytes_sha1_in = System.Text.UTF8Encoding.Default.GetBytes(strYW);
+            byte[] bytes_sha1_out = sha1.ComputeHash(bytes_sha1_in);
+            string str_sha1_out = BitConverter.ToString(bytes_sha1_out);
+            str_sha1_out = str_sha1_out.Replace("-", "").ToLower();
+
+
+            sdkSign.signature = str_sha1_out;
+
+            return sdkSign;
+        }
+
+    }
+
+}

2c6af93e39ba568633100abf8f4b149644797ece
ZhangXiaoDong <dashiell.zhang@qq.com>
2019-11-17 15:04:24 +0800

    调整 CUD_User 基类 修复该类缺失 UpdateTime 字段问题

--- Models/DataBases/WebCore/Bases/CUD_User.cs
+++ Models/DataBases/WebCore/Bases/CUD_User.cs
@@ -7,11 +7,22 @@ using System.Text;
 namespace Models.DataBases.WebCore.Bases
 {
 
+
     /// <summary>
     /// 创建，编辑，删除，并关联了用户
     /// </summary>
-    public class CUD_User:CD_User
+    public class CUD_User : CUD
     {
+
+
+        /// <summary>
+        /// 创建人ID
+        /// </summary>
+
+        [MaxLength(36)]
+        public string CreateUserId { get; set; }
+        public TUser CreateUser { get; set; }
+
 
         /// <summary>
         /// 编辑人ID
@@ -19,6 +30,15 @@ namespace Models.DataBases.WebCore.Bases
         [MaxLength(36)]
         public string UpdateUserId { get; set; }
         public TUser UpdateUser { get; set; }
+
+
+        /// <summary>
+        /// 删除人ID
+        /// </summary>
+        [MaxLength(36)]
+        public string DeleteUserId { get; set; }
+        public TUser DeleteUser { get; set; }
+
 
     }
 }

b6c9b8fe3113dc31f596a9ac65957b09ec86833e
ZhangXiaoDong <dashiell.zhang@qq.com>
2020-05-07 20:44:15 +0800

    修复大文件上传失败的问题

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,11 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = long.MaxValue;
+            });
 
             services.Configure<KestrelServerOptions>(options =>
             {

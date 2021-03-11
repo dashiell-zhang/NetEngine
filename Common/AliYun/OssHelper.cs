28c8b07de893447991ffa9f00752c25b7010b067
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-03-11 14:41:16 +0800

    OSS 文件删除方法实现，OSS Bucket 创建、删除方法实现

--- Common/AliYun/OssHelper.cs
+++ Common/AliYun/OssHelper.cs
@@ -1,6 +1,8 @@
 ﻿using Aliyun.OSS;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Common.AliYun
 {
@@ -17,6 +19,8 @@ namespace Common.AliYun
         }
 
 
+
+
         public OssHelper(string in_endpoint, string in_accessKeyId, string in_accessKeySecret, string in_bucketName)
         {
             endpoint = in_endpoint;
@@ -24,6 +28,8 @@ namespace Common.AliYun
             accessKeySecret = in_accessKeySecret;
             bucketName = in_bucketName;
         }
+
+
 
 
         /// <summary>
@@ -99,5 +105,162 @@ namespace Common.AliYun
                 return false;
             }
         }
+
+
+
+
+        /// <summary>
+        /// 单个文件删除方法
+        /// </summary>
+        /// <param name="remotePath">远程文件地址</param>
+        /// <returns></returns>
+        public bool FileDelete(string remotePath)
+        {
+
+            try
+            {
+
+                var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+
+                client.DeleteObject(bucketName, remotePath);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Delete object failed. {0}", ex.Message);
+                return false;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 文件批量删除方法
+        /// </summary>
+        /// <param name="remotePathList">远程文件地址集合</param>
+        /// <returns></returns>
+        public bool FileBatchDelete(List<string> remotePathList)
+        {
+
+            try
+            {
+                var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+
+                var request = new DeleteObjectsRequest(bucketName, remotePathList, true);
+
+                client.DeleteObjects(request);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Delete objects failed. {0}", ex.Message);
+                return false;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 创建存储空间（Bucket）
+        /// </summary>
+        /// <param name="bucketName"></param>
+        public bool BucketCreate(string bucketName)
+        {
+            try
+            {
+                // 初始化OssClient。
+                var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+
+                var exist = client.DoesBucketExist(bucketName);
+
+                if (exist == false)
+                {
+                    var request = new CreateBucketRequest(bucketName);
+
+                    //设置存储空间访问权限ACL。
+                    request.ACL = CannedAccessControlList.PublicRead;
+
+                    //设置数据容灾类型。
+                    request.DataRedundancyType = DataRedundancyType.LRS;
+
+                    // 创建存储空间。
+                    client.CreateBucket(request);
+                }
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Create bucket failed. {0}", ex.Message);
+
+                return false;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 删除存储空间（Bucket）
+        /// </summary>
+        /// <param name="bucketName"></param>
+        /// <param name="isCoerce">是否强制删除</param>
+        public bool BucketDelete(string bucketName, bool isCoerce = false)
+        {
+            try
+            {
+
+                // 初始化OssClient。
+                var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+
+                if (isCoerce)
+                {
+
+                    while (true)
+                    {
+                        var listObjectsRequest = new ListObjectsRequest(bucketName);
+
+                        var result = client.ListObjects(listObjectsRequest);
+
+                        var keys = result.ObjectSummaries.ToList().Select(t => t.Key).ToList();
+
+                        if (keys.Count == 0)
+                        {
+                            break;
+                        }
+
+                        var request = new DeleteObjectsRequest(bucketName, keys, true);
+                        client.DeleteObjects(request);
+                    }
+
+                }
+
+
+
+                var exist = client.DoesBucketExist(bucketName);
+
+                if (exist == true)
+                {
+                    client.DeleteBucket(bucketName);
+                }
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Delete bucket failed. {0}", ex.Message);
+
+                return false;
+            }
+        }
+
+
+
     }
 }

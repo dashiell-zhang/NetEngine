bad824181096494d82f0cbabbe0c8d36cca73b3f
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-09-14 20:09:58 +0800

    友链管理实现

---
+++ AdminApp/Models/Link/dtoUpdateLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApp.Models.Link
+{
+
+    /// <summary>
+    /// 更新友情链接
+    /// </summary>
+    public class dtoUpdateLink
+    {
+
+
+        /// <summary>
+        /// 标识ID
+        /// </summary>
+        public Guid Id { get; set; }
+
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+
+
+        /// <summary>
+        /// 网址
+        /// </summary>
+        public string Url { get; set; }
+
+
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int Sort { get; set; }
+
+
+    }
+}

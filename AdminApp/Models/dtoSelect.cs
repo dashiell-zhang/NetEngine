e3b08cb4aea8a83a63bdcd7fabaef4e6e9513d97
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-09-15 22:10:53 +0800

    优化栏目管理，实现子栏目维护

---
+++ AdminApp/Models/dtoSelect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminApp.Models
+{
+    public class dtoSelect
+    {
+
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public Guid Value { get; set; }
+
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public string Label { get; set; }
+
+
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool IsDisabled { get; set; }
+
+
+    }
+}

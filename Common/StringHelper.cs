3896742794130d5d182281e302c17af80f047038
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-12-09 14:37:06 +0800

    重写 StringHelper.NoEmoji 方法

--- Common/StringHelper.cs
+++ Common/StringHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -159,15 +160,21 @@ namespace Common
         /// <returns></returns>
         public static string NoEmoji(string value)
         {
-            foreach (var a in value)
+
+            string emojiUnicodeBytesStr = "[4848,14254,6032,7332,420,22732,350,6148,1690,1740,14833,14933,15033,15133,15233,15333,16933,17033,4035,20735,23735,23835,23935,24135,24235,24835,24935,25035,17037,17137,18237,19237,25137,25237,38,138,238,338,438,1438,1738,2438,2938,3238,3438,3538,3838,4238,4638,4738,5638,5738,5838,6438,6638,9538,9638,9938,10138,10238,10438,12338,12638,14638,14838,14938,15038,15138,15338,15538,15638,16038,16738,17638,17738,20038,20738,20938,21138,23338,24038,24138,24438,24738,24838,24938,239,839,939,1239,1339,1539,1839,2039,2239,2939,3339,5139,5239,6839,7139,9939,10039,16139,5241,5341,543,643,743,253255,480,490,500,510,520,530,540,550,560,570,5733,19436,3433,15150,15350,15254,1139,1039,2138,23438,24238,25038,25338,14738,24538,2735,24335,2635,24035,8043,19738,2038,16138,19638,4039,18938,19038,24338,12738,21238,7238,7338,7438,7538,7638,7738,7838,7938,8038,8138,8238,8338,20638,23335,23435,23535,23635,14939,15039,15139,8339,8439,8539,8739,8543,539,7639,7839,17639,19139,17138,17038,2743,2843,25437,25337,1332]";
+
+            var emojiUnicodeBytesList = Json.JsonHelper.JsonToObject<List<int>>(emojiUnicodeBytesStr);
+
+            foreach (var v in value)
             {
-                byte[] bts = Encoding.UTF32.GetBytes(a.ToString());
+                var emojiUnicodeBytes = Encoding.Unicode.GetBytes(v.ToString()).ToList();
 
-                if (bts[0].ToString() == "253" && bts[1].ToString() == "255")
+                var emojiUnicodeBytesInt = int.Parse(string.Join("", emojiUnicodeBytes));
+
+                if (emojiUnicodeBytesList.Contains(emojiUnicodeBytesInt))
                 {
-                    value = value.Replace(a.ToString(), "");
+                    value = value.Replace(v.ToString(), "");
                 }
-
             }
 
             return value;

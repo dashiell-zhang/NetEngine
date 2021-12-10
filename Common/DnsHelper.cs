2fd0498234cf85756f03375748e6c3418a16f55a
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-12-10 17:01:49 +0800

    DnsHelper 实现

---
+++ Common/DnsHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    public class DnsHelper
+    {
+
+
+
+        /// <summary>
+        /// 获取指定域名的 Text 记录
+        /// </summary>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        public static List<string> GetDomainText(string domain)
+        {
+            string key = CryptoHelper.GetMd5("dns:" + domain + ":txt");
+
+            string retStr = CacheHelper.GetString(key);
+
+            if (string.IsNullOrEmpty(retStr))
+            {
+                string url = "https://dns.alidns.com/resolve?name=" + domain + "&type=TXT";
+                retStr = HttpHelper.Get(url);
+
+                CacheHelper.SetString(key, retStr, TimeSpan.FromMinutes(10));
+            }
+
+            var retData = Json.JsonHelper.JsonToObject<DnsReturn>(retStr);
+
+            return retData.Answer.ToList().Select(t => t.data.Replace("\"", "")).ToList();
+        }
+
+
+
+        private class DnsReturn
+        {
+            public int Status { get; set; }
+
+            public bool TC { get; set; }
+
+            public bool RD { get; set; }
+
+            public bool RA { get; set; }
+
+            public bool AD { get; set; }
+
+            public bool CD { get; set; }
+
+            public DomainTextRetQuestion Question { get; set; }
+
+            public class DomainTextRetQuestion
+            {
+                public string name { get; set; }
+                public int type { get; set; }
+            }
+
+            public DomainTextRetAnswer[] Answer { get; set; }
+
+            public class DomainTextRetAnswer
+            {
+                public string name { get; set; }
+                public int TTL { get; set; }
+                public int type { get; set; }
+                public string data { get; set; }
+            }
+
+        }
+
+
+
+
+
+    }
+}

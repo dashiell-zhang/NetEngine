3c2a3a02a2d07d6339411a406134b01570519fbf
ZhangXiaoDong <dashiell.zhang@qq.com>
2021-12-10 12:22:37 +0800

    加入 https 指纹验签示例

--- TaskService/Program.cs
+++ TaskService/Program.cs
@@ -133,6 +133,22 @@ namespace TaskService
                         ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
                     });
 
+
+                    services.AddHttpClient("xxx.com", options =>
+                    {
+                        options.DefaultRequestVersion = new Version("2.0");
+                        options.DefaultRequestHeaders.Add("Accept", "*/*");
+                        options.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
+                        options.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
+                    }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+                    {
+                        AllowAutoRedirect = false,
+                        ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+                        {
+                            return string.Equals(cert.Thumbprint, "xxxxxx", StringComparison.OrdinalIgnoreCase);
+                        }
+                    });
+
                 });
 
 

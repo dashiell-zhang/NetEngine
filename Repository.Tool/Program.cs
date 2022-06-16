635a4b4aae77583d46bc38598a0fa4d3bacc351a
ZhangXiaoDong <dashiell.zhang@qq.com>
2022-06-16 22:42:48 +0800

    为 Repository 构建单独的迁移项目 Repository.Tool

---
+++ Repository.Tool/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Repository.Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddDbContext<Database.DatabaseContext>(options =>
+                        {
+                            options.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=webcore;User ID=sa;Password=123456;Max Pool Size=100;Encrypt=True", x => x.MigrationsAssembly("Repository.Tool"));
+                        });
+                    }).Build();
+
+            host.Run();
+        }
+    }
+}

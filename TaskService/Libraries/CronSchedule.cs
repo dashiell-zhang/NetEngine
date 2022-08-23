cc3b49fb86f7506568a7b25ad3d7497fce7abb25
ZhangXiaoDong <dashiell.zhang@qq.com>
2022-08-23 22:41:55 +0800

    优化 TaskService 采用 Cron 计划模式执行

---
+++ TaskService/Libraries/CronSchedule.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+
+namespace TaskService.Libraries
+{
+    public class CronSchedule
+    {
+
+
+        public static async void Builder(CancellationToken stoppingToken, string cronExpression, Action action)
+        {
+            var nextTime = CronHelper.GetNextOccurrence(cronExpression).ToString("yyyy-MM-dd HH:mm:ss");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var nowTime = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+
+                if (nextTime == nowTime)
+                {
+                    nextTime = CronHelper.GetNextOccurrence(cronExpression).ToString("yyyy-MM-dd HH:mm:ss");
+
+                    action();
+                }
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+    }
+}
--- TaskService/Tasks/DemoTask.cs
+++ TaskService/Tasks/DemoTask.cs
@@ -1,6 +1,7 @@
 ﻿using Common;
 using DistributedLock;
 using Repository.Database;
+using TaskService.Libraries;
 
 namespace TaskService.Tasks
 {
@@ -9,33 +10,44 @@ namespace TaskService.Tasks
 
         private readonly IServiceProvider serviceProvider;
 
-        public DemoTask(IServiceProvider serviceProvider)
+        private readonly ILogger logger;
+
+        private readonly IDHelper idHelper;
+
+
+        public DemoTask(IServiceProvider serviceProvider, ILogger<DemoTask> logger, IDHelper idHelper)
         {
             this.serviceProvider = serviceProvider;
+            this.logger = logger;
+            this.idHelper = idHelper;
         }
 
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            using Timer timer = new(Run, null, 0, 1000);
+            CronSchedule.Builder(stoppingToken, "0/5 * * * * ?", Run);
+
             await Task.Delay(-1, stoppingToken);
         }
 
 
 
-        private void Run(object? state)
+        private void Run()
         {
-            var idHelper = serviceProvider.GetRequiredService<IDHelper>();
-            var logger = serviceProvider.GetRequiredService<ILogger<DemoTask>>();
+            try
+            {
+                using var scope = serviceProvider.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+                var distLock = scope.ServiceProvider.GetRequiredService<IDistributedLock>();
 
-            using var scope = serviceProvider.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-            var distLock = scope.ServiceProvider.GetRequiredService<IDistributedLock>();
+                logger.LogInformation("HelloWord{Id}", idHelper.GetId());
 
-            logger.LogInformation("HelloWord{Id}", idHelper.GetId());
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "DemoTask.Run");
+            }
 
         }
-
-
     }
 }

c6c217dc655562f011cc548dd4e2c2a0f56d6dc0
ZhangXiaoDong <dashiell.zhang@qq.com>
2024-07-29 11:06:07 +0800

    feat: ETag 计算过滤器实现

---
+++ WebAPIBasic/Filters/ETagFilter.cs
@@ -0,0 +1,49 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
+
+namespace WebAPIBasic.Filters
+{
+
+
+    /// <summary>
+    /// ETag计算过滤器
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ETagFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var request = context.HttpContext.Request;
+            var response = context.HttpContext.Response;
+
+            if (response.StatusCode == 200)
+            {
+                if (context.Result is ObjectResult result)
+                {
+                    if (result.Value != null)
+                    {
+                        var etag = CryptoHelper.MD5HashData(JsonHelper.ObjectToJson(result.Value));
+
+                        if (request.Headers.TryGetValue(HeaderNames.IfNoneMatch, out var incomingEtag) && incomingEtag == etag)
+                        {
+                            context.Result = new StatusCodeResult(304);
+                        }
+                        else
+                        {
+                            response.Headers[HeaderNames.ETag] = etag;
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}

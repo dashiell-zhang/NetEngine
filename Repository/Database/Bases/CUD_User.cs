0a70c390af7d8cede69f34929bc13135c81db9a8
ZhangXiaoDong <dashiell.zhang@qq.com>
2024-10-08 20:32:56 +0800

    fix: CUD_User

--- Repository/Database/Bases/CUD_User.cs
+++ Repository/Database/Bases/CUD_User.cs
@@ -10,6 +10,12 @@ namespace Repository.Database.Bases
     public class CUD_User : CD_User
     {
 
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTimeOffset? UpdateTime { get; set; }
+
+
 
         /// <summary>
         /// 编辑人ID

6ad3f37b7eb8130328372af5dea78fda8818037d
ZhangXiaoDong <dashiell.zhang@qq.com>
2024-10-10 16:31:38 +0800

    pref: GetClaimOrDefault 支持 null 类型

--- WebAPIBasic/Libraries/ClaimsPrincipalExtension.cs
+++ WebAPIBasic/Libraries/ClaimsPrincipalExtension.cs
@@ -12,46 +12,51 @@ namespace WebAPIBasic.Libraries
             {
                 object value;
 
-                switch (Type.GetTypeCode(typeof(T)))
+                var type = typeof(T);
+
+                if (typeof(T) == typeof(Guid) || typeof(T) == typeof(Guid?))
                 {
-                    case TypeCode.String:
-                        value = valueStr;
-                        break;
-
-                    case TypeCode.Boolean:
-                        value = bool.Parse(valueStr);
-                        break;
-
-                    case TypeCode.Decimal:
-                        value = decimal.Parse(valueStr);
-                        break;
-
-                    case TypeCode.Int32:
-                        value = int.Parse(valueStr);
-                        break;
-
-                    case TypeCode.Int64:
-                        value = long.Parse(valueStr);
-                        break;
-
-                    case TypeCode.DateTime:
-                        value = DateTime.Parse(valueStr);
-                        break;
-
-                    default:
-                        if (typeof(T) == typeof(Guid))
-                        {
-                            value = Guid.Parse(valueStr);
-                        }
-                        else if (typeof(T) == typeof(DateTimeOffset))
-                        {
-                            value = DateTimeOffset.Parse(valueStr);
-                        }
-                        else
-                        {
-                            value = Convert.ChangeType(valueStr, typeof(T));
-                        }
-                        break;
+                    value = Guid.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(long) || typeof(T) == typeof(long?))
+                {
+                    value = long.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(string))
+                {
+                    value = valueStr;
+                }
+                else if (typeof(T) == typeof(DateTime) || typeof(T) == typeof(DateTime?))
+                {
+                    value = DateTime.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(DateTimeOffset) || typeof(T) == typeof(DateTimeOffset?))
+                {
+                    value = DateTimeOffset.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(int) || typeof(T) == typeof(int?))
+                {
+                    value = int.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(bool) || typeof(T) == typeof(bool?))
+                {
+                    value = bool.Parse(valueStr);
+                }
+                else if (typeof(T) == typeof(decimal) || typeof(T) == typeof(decimal?))
+                {
+                    value = decimal.Parse(valueStr);
+                }
+                else
+                {
+                    if (IsNullable(type))
+                    {
+                        var parentType = type.GenericTypeArguments[0];
+                        value = Convert.ChangeType(valueStr, parentType);
+                    }
+                    else
+                    {
+                        value = Convert.ChangeType(valueStr, typeof(T));
+                    }
                 }
 
                 return (T)value;
@@ -60,6 +65,24 @@ namespace WebAPIBasic.Libraries
             {
                 throw new Exception($"获取 {key} 失败，{key} 类型转换失败");
             }
+
+
+            bool IsNullable(Type type)
+            {
+                bool isNullable = false;
+
+                if (Nullable.GetUnderlyingType(type) != null)
+                {
+                    isNullable = true;
+                }
+                else if (!type.IsValueType)
+                {
+                    isNullable = true;
+                }
+
+                return isNullable;
+            }
+
         }
 
 

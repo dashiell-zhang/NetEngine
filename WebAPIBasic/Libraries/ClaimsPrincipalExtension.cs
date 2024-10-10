2570d0c3f3d03fc24299d456685e16f4f43503d9
ZhangXiaoDong <dashiell.zhang@qq.com>
2024-10-10 10:16:39 +0800

    pref: 优化 GetClaim 性能对于常见类型执行特定的转换

--- WebAPIBasic/Libraries/ClaimsPrincipalExtension.cs
+++ WebAPIBasic/Libraries/ClaimsPrincipalExtension.cs
@@ -7,27 +7,64 @@ namespace WebAPIBasic.Libraries
 
 
         /// <summary>
-        /// 通过Key获取HttpContext.User.Claims中的信息
+        /// 通过Key获取Claims中的信息
         /// </summary>
-        /// <param name="user"></param>
+        /// <param name="claims"></param>
         /// <param name="key">Claim关键字</param>
         /// <returns></returns>
-        public static T GetClaim<T>(this ClaimsPrincipal user, string key)
+        public static T GetClaim<T>(this ClaimsPrincipal claims, string key) where T : IConvertible
         {
-            var valueStr = user.FindFirst(key)?.Value;
+            var valueStr = claims.FindFirst(key)?.Value;
 
-            var value = Convert.ChangeType(valueStr, typeof(T));
-
-            if (value != null)
+            if (valueStr != null)
             {
-                return (T)value;
+                try
+                {
+                    object value;
+
+                    switch (Type.GetTypeCode(typeof(T)))
+                    {
+                        case TypeCode.String:
+                            value = valueStr;
+                            break;
+
+                        case TypeCode.Boolean:
+                            value = bool.Parse(valueStr);
+                            break;
+
+                        case TypeCode.Int32:
+                            value = int.Parse(valueStr);
+                            break;
+
+                        case TypeCode.Int64:
+                            value = long.Parse(valueStr);
+                            break;
+
+                        default:
+                            if (typeof(T) == typeof(Guid))
+                            {
+                                value = Guid.Parse(valueStr);
+                            }
+                            else
+                            {
+                                value = Convert.ChangeType(valueStr, typeof(T));
+                            }
+                            break;
+                    }
+
+                    return (T)value;
+                }
+                catch
+                {
+                    throw new Exception($"获取 {key} 失败，{key} 类型转换失败");
+                }
             }
             else
             {
-                throw new Exception($"获取{key}失败");
+                throw new Exception($"获取 {key} 失败，{key} 不存在");
             }
-
         }
+
 
     }
 }

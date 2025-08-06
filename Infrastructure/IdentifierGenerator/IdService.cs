2c79874ea645a5d7860e467cf7753fc8baa6ad30
ZhangXiaoDong <dashiell.zhang@qq.com>
2025-08-06 21:33:12 +0800

    pref: 优化 idservice 的逻辑采用 long 位运算提升性能

--- Infrastructure/IdentifierGenerator/IdService.cs
+++ Infrastructure/IdentifierGenerator/IdService.cs
@@ -11,35 +11,25 @@ namespace IdentifierGenerator
 
         public IdService(IOptionsMonitor<IdSetting> config, IDistributedLock distributedLock, IDistributedCache distributedCache)
         {
-
             if (config.CurrentValue.DataCenterId == null || config.CurrentValue.MachineId == null)
             {
                 using (distributedLock.LockAsync("IdentifierGenerator").Result)
                 {
                     Random rand = new();
-
                     string key = "";
-
                     int tryCount = 0;
-
                     do
                     {
                         tryCount++;
-
                         if (tryCount > 10)
                         {
                             throw new Exception("Id生成器注册DataCenterId和MachineId失败");
                         }
-
-                        int combinedNumber = rand.Next(0, 1025);
-
-                        config.CurrentValue.DataCenterId = (combinedNumber >> 5) & 31; //dataCenterId 取组合数右移5位后的低5位
-                        config.CurrentValue.MachineId = combinedNumber & 31; //machineId 取组合数的低5位
-
+                        int combinedNumber = rand.Next(0, 1025); // 1024 = 2^10
+                        config.CurrentValue.DataCenterId = (combinedNumber >> 5) & 31; // dataCenterId 取组合数右移5位后的低5位
+                        config.CurrentValue.MachineId = combinedNumber & 31; // machineId 取组合数的低5位
                         key = "IdentifierGenerator-" + config.CurrentValue.DataCenterId + ":" + config.CurrentValue.MachineId;
-
                     } while (distributedCache.IsContainKey(key));
-
                     distributedCache.Set(key, "", TimeSpan.FromHours(1));
                 }
             }
@@ -49,19 +39,20 @@ namespace IdentifierGenerator
                 dataCenterId = config.CurrentValue.DataCenterId.Value;
                 machineId = config.CurrentValue.MachineId.Value;
 
-                maxMachineId = -1L ^ -1L << (int)machineIdBits;
-                maxDataCenterId = -1L ^ -1L << (int)dataCenterIdBits;
+                // 使用位运算直接计算掩码和最大值，更高效且清晰
+                maxMachineId = -1L ^ (-1L << (int)machineIdBits);
+                maxDataCenterId = -1L ^ (-1L << (int)dataCenterIdBits);
+                sequenceMask = -1L ^ (-1L << (int)sequenceBits);
+
+                // 计算移位数
                 machineIdShift = sequenceBits;
                 dataCenterIdShift = sequenceBits + machineIdBits;
                 timestampLeftShift = sequenceBits + machineIdBits + dataCenterIdBits;
-                sequenceMask = -1L ^ -1L << (int)sequenceBits;
-
 
                 if (dataCenterId < 0 || dataCenterId > maxDataCenterId)
                 {
                     throw new Exception("数据中心ID异常");
                 }
-
                 if (machineId < 0 || machineId > maxMachineId)
                 {
                     throw new Exception("机器码ID异常");
@@ -76,23 +67,25 @@ namespace IdentifierGenerator
 
         private readonly long machineId;
         private readonly long dataCenterId;
-        private long sequence = 0L;//计数从零开始
-        private long lastTimestamp = -1L;//最后时间戳
+        private long sequence = 0L; //计数从零开始
+        private long lastTimestamp = -1L; //最后时间戳
 
-        //twepoch 1640995200000L 为 UTC 2022-01-01 00:00 , sequenceBits 调整为11位，所以时间戳可用 42位，未来139年 可用
-        private readonly long twepoch = 1640995200000L; //唯一时间随机量，这是一个避免重复的随机量，自行设定不要大于当前时间戳
+        private readonly long twepoch = 1640995200000L; // UTC 2022-01-01 00:00:00
 
-        private readonly long machineIdBits = 5L; //机器码字节数
-        private readonly long dataCenterIdBits = 5L;//数据字节数
-        private readonly long maxMachineId; //最大机器ID
-        private readonly long maxDataCenterId;//最大数据ID
-        private readonly long sequenceBits = 11L; //计数器字节数，11个字节用来保存计数码，每毫秒可以生成2047个ID
-        private readonly long machineIdShift; //机器码数据左移位数，就是后面计数器占用的位数
+        private readonly long machineIdBits = 5L;
+        private readonly long dataCenterIdBits = 5L;
+        private readonly long sequenceBits = 11L;
+
+        private readonly long maxMachineId;
+        private readonly long maxDataCenterId;
+        private readonly long sequenceMask;
+
+        private readonly long machineIdShift;
         private readonly long dataCenterIdShift;
-        private readonly long timestampLeftShift;//时间戳左移动位数就是机器码+计数器总字节数+数据字节数
-        private readonly long sequenceMask; //一微秒内可以产生计数，如果达到该值则等到下一微妙在进行生成
-        private readonly Lock syncRoot = new();//加锁对象
+        private readonly long timestampLeftShift;
 
+        // 使用 object 作为锁对象是更通用的做法
+        private readonly object syncRoot = new object();
 
 
         /// <summary>
@@ -103,93 +96,96 @@ namespace IdentifierGenerator
         {
             lock (syncRoot)
             {
-                long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                long timestamp = GetCurrentTimestamp();
 
+                // 当发生时钟回拨时，抛出异常
+                if (timestamp < lastTimestamp)
+                {
+                    throw new Exception($"时钟回拨。拒绝为 {lastTimestamp - timestamp} 毫秒生成ID");
+                }
+
+                // 如果是同一毫秒内，则进行序列号递增
                 if (lastTimestamp == timestamp)
                 {
-                    //同一微妙中生成ID
-                    sequence = sequence + 1 & sequenceMask; //用&运算计算该微秒内产生的计数是否已经到达上限
+                    // 通过位运算增加序列号，并防止溢出
+                    sequence = (sequence + 1) & sequenceMask;
+                    // 如果序列号溢出，则等待到下一毫秒
                     if (sequence == 0)
                     {
-                        //一微妙内产生的ID计数已达上限，等待下一微妙
-                        int i = 10;
-
-                        do
-                        {
-                            timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                            i--;
-                        } while (timestamp <= lastTimestamp && i > 0);
+                        timestamp = WaitNextMillis(lastTimestamp);
                     }
                 }
                 else
                 {
-                    //不同微秒生成ID
+                    // 时间戳改变，序列号重置
                     sequence = 0L;
                 }
 
-                if (timestamp < lastTimestamp)
-                {
-                    throw new Exception("时间戳比上一次生成ID时时间戳还小，故异常");
-                }
+                // 更新最后的时间戳
+                lastTimestamp = timestamp;
 
-                lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
-
-                long Id = timestamp - twepoch << (int)timestampLeftShift
-                    | dataCenterId << (int)dataCenterIdShift
-                    | machineId << (int)machineIdShift
-                    | sequence;
-                return Id;
+                // 通过位运算拼接ID
+                // 逻辑：(时间戳 - 起始时间) 左移 | 数据中心ID 左移 | 机器ID 左移 | 序列号
+                return ((timestamp - twepoch) << (int)timestampLeftShift)
+                       | (dataCenterId << (int)dataCenterIdShift)
+                       | (machineId << (int)machineIdShift)
+                       | sequence;
             }
         }
-
 
 
         /// <summary>
-        /// 通过ID获取其中的时间戳
+        /// 通过ID获取其中的UTC时间
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">雪花ID</param>
+        /// <returns>ID中包含的UTC时间</returns>
         public DateTimeOffset GetTimeById(long id)
         {
-            var idStr2 = Convert.ToString(id, 2);
-
-            if (idStr2.Length < 63)
-            {
-                do
-                {
-                    idStr2 = "0" + idStr2;
-                } while (idStr2.Length != 63);
-            }
-
-            var timeBits = 64 - machineIdBits - dataCenterIdBits - sequenceBits - 1;
-
-            var timeStr2 = idStr2[..Convert.ToInt32(timeBits)];
-
-            var timeJsStamp = Convert.ToInt64(timeStr2, 2);
-
-            var twepochTime = DateTimeOffset.FromUnixTimeMilliseconds(twepoch);
-
-            DateTime startTime = new(twepochTime.Year, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return startTime.AddMilliseconds(timeJsStamp);
+            long timestamp = (id >> (int)timestampLeftShift) + twepoch;
+            return DateTimeOffset.FromUnixTimeMilliseconds(timestamp);
         }
-
 
 
         /// <summary>
         /// 获取指定时间开始的最小Id
         /// </summary>
-        /// <returns></returns>
+        /// <param name="startTime">指定的起始时间</param>
+        /// <returns>该毫秒对应的最小ID</returns>
         public long GetMinIdByTime(DateTimeOffset startTime)
         {
             long timestamp = startTime.ToUnixTimeMilliseconds();
 
-            lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
-            long Id = timestamp - twepoch << (int)timestampLeftShift
-                | 0L << (int)dataCenterIdShift
-                | 0L << (int)machineIdShift
-                | 0;
-            return Id;
+            // 最小ID即为该时间戳左移相应位数，数据中心、机器码和序列号部分都为0。
+
+            return (timestamp - twepoch) << (int)timestampLeftShift;
         }
 
+
+        /// <summary>
+        /// 阻塞到下一个毫秒，直到获得新的时间戳
+        /// </summary>
+        /// <param name="lastTimestamp">上一次生成ID的时间戳</param>
+        /// <returns>新的时间戳</returns>
+        private long WaitNextMillis(long lastTimestamp)
+        {
+            long timestamp = GetCurrentTimestamp();
+            while (timestamp <= lastTimestamp)
+            {
+                // 选择短暂休眠来避免CPU空转，对于高并发场景更为友好
+                Thread.Sleep(1);
+                timestamp = GetCurrentTimestamp();
+            }
+            return timestamp;
+        }
+
+
+        /// <summary>
+        /// 获取当前UTC时间戳（毫秒）
+        /// </summary>
+        /// <returns></returns>
+        private long GetCurrentTimestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
     }
 }

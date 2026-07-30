342dbb172262ee28f31e80da0bde283a7f26cdaf
Xiaodong.Zhang <dashiell.zhang@qq.com>
2026-07-30 11:43:11 +0800

    perf: 完善 XmlHelper 序列化命名空间处理与反序列化安全配置

--- Infrastructure/Common/XmlHelper.cs
+++ Infrastructure/Common/XmlHelper.cs
@@ -3,6 +3,10 @@ using System.Xml;
 using System.Xml.Serialization;
 
 namespace Common;
+
+/// <summary>
+/// 提供XML序列化和反序列化能力
+/// </summary>
 public class XmlHelper
 {
 
@@ -21,13 +25,16 @@ public class XmlHelper
             OmitXmlDeclaration = true
         };
 
-        using var xmlWriter = XmlWriter.Create(memoryStream, xmlWriterSettings);
         //去除默认命名空间xmlns:xsd和xmlns:xsi
         XmlSerializerNamespaces ns = new();
         ns.Add("", "");
 
         XmlSerializer xmlSerializer = new(obj.GetType());
-        xmlSerializer.Serialize(xmlWriter, obj);
+
+        using (var xmlWriter = XmlWriter.Create(memoryStream, xmlWriterSettings))
+        {
+            xmlSerializer.Serialize(xmlWriter, obj, ns);
+        }
 
         memoryStream.Position = 0;
         using StreamReader sr = new(memoryStream);
@@ -47,9 +54,21 @@ public class XmlHelper
     public static T XmlToObject<T>(string xmlText)
     {
         using StringReader stringReader = new(xmlText);
+        XmlReaderSettings xmlReaderSettings = new()
+        {
+            DtdProcessing = DtdProcessing.Prohibit,
+            XmlResolver = null
+        };
+        using XmlReader xmlReader = XmlReader.Create(stringReader, xmlReaderSettings);
         XmlSerializer xmlSerializer = new(typeof(T));
+        object? result = xmlSerializer.Deserialize(xmlReader);
 
-        return (T)xmlSerializer.Deserialize(stringReader)!;
+        if (result is T value)
+        {
+            return value;
+        }
+
+        throw new InvalidOperationException($"XML反序列化结果不能转换为 {typeof(T).FullName}");
     }
 
 
